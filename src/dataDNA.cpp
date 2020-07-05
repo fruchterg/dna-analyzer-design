f19e9d3619e5d0d71a0b2d4498ed0f6cec3ebd1c
gilifruchter <z618149941@gmail.com>
2020-07-05 16:48:14 -0700

    class dataDNA

--- src/dataDNA.cpp
+++ src/dataDNA.cpp
@@ -3,3 +3,18 @@
 //
 
 #include "dataDNA.h"
+static std::map<IdDna,Dna> getIdDNA()
+{
+    static std::map<IdDna,Dna> m_mId;
+    return m_mId;
+}
+static std::map<NameDna ,Dna&> getNameDNA()
+{
+    static std::map<NameDna,Dna&> m_mName;
+    return  m_mName;
+}
+static std::map<StatusDna,Dna&> getStatusDNA()
+{
+    static std::map<StatusDna,Dna&> m_mStatus;
+    return m_mStatus;
+}
--- src/dataDNA.h
+++ src/dataDNA.h
@@ -5,8 +5,18 @@
 #ifndef DNDSEQUENCE_DATADNA_H
 #define DNDSEQUENCE_DATADNA_H
 
-
+#include <map>
+#include "Dna.h"
+#include "IdDna.h"
+#include "NameDna.h"
+#include "StatusDna.h"
 class dataDNA {
+
+public:
+    static std::map<IdDna,Dna> getIdDNA();
+    static std::map<NameDna ,Dna&> getNameDNA();
+    static std::map<StatusDna,Dna> getStatusDNA();
+
 
 };
 
