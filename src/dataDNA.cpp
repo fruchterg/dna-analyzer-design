232b56122387b4c01f7d9d378a55e662766277c5
gilifruchter <z618149941@gmail.com>
2020-07-06 15:29:39 -0700

    suppor load command

--- src/dataDNA.cpp
+++ src/dataDNA.cpp
@@ -3,14 +3,19 @@
 //
 
 #include "dataDNA.h"
-std::map<size_t ,Dna*> dataDNA::getIdDNA()
+std::map<size_t ,Dna*>& dataDNA::getIdDNA()
 {
     static std::map<size_t ,Dna*> m_mId;
     return m_mId;
 }
- std::map<std::string ,size_t> dataDNA::getNameDNA()
+ std::map<std::string ,size_t>& dataDNA::getNameDNA()
 {
     static std::map<std::string,size_t> m_mName;
     return  m_mName;
 }
 
+/*std::map<StatusDna,Dna&> dataDNA::getStatusDNA()
+{
+    static std::map<StatusDna,Dna&> m_mStatus;
+    return m_mStatus;
+}*/
