155d137b09a212d9b44d18d87197abd5426717ad
gilifruchter <z618149941@gmail.com>
2020-07-06 15:36:01 -0700

    suppor load command

--- src/Ireader.cpp
+++ src/Ireader.cpp
@@ -5,26 +5,9 @@
 #include <fstream>
 #include <iostream>
 #include "Ireader.h"
-////////files/////////
 
 
-Filereader::Filereader(const char* name_file)
-{
-    std::ifstream myfile(name_file);
-    if(myfile.is_open())
-    {
-        getline(myfile,m_data);
-    }
-    else{
-        throw std::ios_base::failure("faild open file");
-    }
-    myfile.close();
 
-}
-std::string Filereader::read()const
-{
-    return m_data;
-}
 
 
 Consolereader::Consolereader()
