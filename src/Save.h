4ca7ef67e698cf362f8333f70d46bcf896284826
gilifruchter <z618149941@gmail.com>
2020-07-07 10:59:00 -0700

    add support save command

---
+++ src/Save.h
@@ -0,0 +1,23 @@
+//
+// Created by a on 7/6/20.
+//
+
+#ifndef DNDSEQUENCE_SAVE_H
+#define DNDSEQUENCE_SAVE_H
+
+#include "Icommand.h"
+
+class Save: public Icommand{
+
+public:
+    Save(const Paramcommand& obj);
+    void run(const Iwriter& writer, dataDNA& containerDna,const Paramcommand& obj);
+
+private:
+    bool isValid(const Paramcommand& obj);
+    //void print(const Iwriter& writer, dataDNA& containerDna);
+
+};
+
+
+#endif //DNDSEQUENCE_SAVE_H
