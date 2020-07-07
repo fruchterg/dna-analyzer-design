a0d8809e722dcc9f27b25af1b3b27b588b6fbb0b
gilifruchter <z618149941@gmail.com>
2020-07-07 16:32:47 -0700

    change start function

--- src/System.cpp
+++ src/System.cpp
@@ -5,23 +5,12 @@
 #include "System.h"
 #include "Iwriter.h"
 #include "Ireader.h"
-#include "Paramcommand.h"
-#include "Managecommand.h"
-void System::start()
+#include "dataDNA.h"
+#include "Terminal.h"
+void System::start(const Iwriter& writer, Ireader& reader)
 {
+    dataDNA containerDna;
+    Terminal Terminal;
+    Terminal.start(writer,reader,containerDna);
 
-    while (1){
-
-
-       Consolereader reader;
-
-       Consolewriter writer;
-
-       Paramcommand parameter(reader.read());
-       Icommand *command = Managecommand::createcommand(parameter);
-        std::string a =  command->run(parameter);
-        //std::cout<<a;
-       writer.write(a);
-
-    }
 }
