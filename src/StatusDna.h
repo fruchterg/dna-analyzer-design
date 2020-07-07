2b552ab67531093af8a69d3ad7b0dac5dc5ed9d4
gilifruchter <z618149941@gmail.com>
2020-07-07 13:37:14 -0700

    add status class

--- src/StatusDna.h
+++ src/StatusDna.h
@@ -6,7 +6,15 @@
 #define DNDSEQUENCE_STATUSDNA_H
 
 
+#include <string>
+#include <list>
 class StatusDna {
+public:
+    StatusDna(const std::string& status);
+
+private:
+    std::string m_status;
+    std::list<std::string> m_statuslist;
 
 };
 
