d24e8ee10ab298b4a19aa6cfe4093f5f641b01ef
gilifruchter <z618149941@gmail.com>
2020-07-07 07:14:46 -0700

    change class dataDNA from static to non stastic class

--- src/dataDNA.cpp
+++ src/dataDNA.cpp
@@ -3,7 +3,7 @@
 //
 
 #include "dataDNA.h"
-std::map<size_t ,Dna*>& dataDNA::getIdDNA()
+/*std::map<size_t ,Dna*>& dataDNA::getIdDNA()
 {
     static std::map<size_t ,Dna*> m_mId;
     return m_mId;
@@ -12,7 +12,23 @@ std::map<size_t ,Dna*>& dataDNA::getIdDNA()
 {
     static std::map<std::string,size_t> m_mName;
     return  m_mName;
+}*/
+
+//////function dataDna///////////////
+void dataDNA::addDna(Dna* newDna)
+{
+    m_mapIdDna.insert(std::pair<size_t, Dna*>(Dna::getId(),newDna));
+    m_mapNameDna.insert(std::pair<std::string, size_t>(newDna->getName(), Dna::getId()));
+
 }
+
+std::map<size_t ,Dna*>& dataDNA::getMap()
+{
+    return m_mapIdDna;
+}
+
+
+
 
 /*std::map<StatusDna,Dna&> dataDNA::getStatusDNA()
 {
