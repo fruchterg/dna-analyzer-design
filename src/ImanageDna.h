7b5b1f1fb6b59195c028ffe4fdb8a2ce421866cd
gilifruchter <z618149941@gmail.com>
2020-07-07 16:29:16 -0700

    add ImanageDna  class

---
+++ src/ImanageDna.h
@@ -0,0 +1,14 @@
+//
+// Created by a on 7/7/20.
+//
+
+#ifndef DNDSEQUENCE_MANAGEDNA_H
+#define DNDSEQUENCE_MANAGEDNA_H
+
+#include "Icommand.h"
+class ImanageDna: public Icommand {
+
+};
+
+
+#endif //DNDSEQUENCE_MANAGEDNA_H
