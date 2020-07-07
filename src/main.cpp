9115206baf57d73a93693021ae9b8e9d4b5f979d
gilifruchter <z618149941@gmail.com>
2020-07-07 16:39:50 -0700

    Add support for choosing the type of writing and reading

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,15 @@
 // Created by a on 7/5/20.
 //
 #include "System.h"
+#include "Iwriter.h"
+#include "Ireader.h"
+
 int main()
 {
-
+    Consolewriter writer;
+    Consolereader reader;
     System program;
-    program.start();
+    program.start(writer,reader);
 
     return 0;
 }
