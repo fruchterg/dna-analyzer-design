bd71d39e65664f6adc151372a81a4efb214bf2e4
gilifruchter <z618149941@gmail.com>
2020-07-08 13:58:16 -0700

    check validation

--- src/Load.cpp
+++ src/Load.cpp
@@ -7,9 +7,9 @@
 #include "Load.h"
 #include "rawDnaReader.h"
 
-bool Load::isValid(const Paramcommand& obj)
+bool Load::isValid(const Paramcommand& param)
 {
-    return true;
+    return ((2==param.getParam().size()||(param.getParam().size()==3&&(param.getParam()[2][0]=='@'))));
 }
 
 Load::Load(const Paramcommand& param)
@@ -25,6 +25,7 @@ void Load::run(const Iwriter& writer, dataDNA& containerDna,const Paramcommand& 
     rawDnaReader myfile(param.getParam()[1]);
     myfile.read();
     std::string dnaName;
+    std::stringstream temp;
 
     if(param.getParam().size()<3)
     {
@@ -32,10 +33,15 @@ void Load::run(const Iwriter& writer, dataDNA& containerDna,const Paramcommand& 
     }
     else
     {
-        dnaName = param.getParam()[2];
+        dnaName = param.getParam()[2].substr(1);;
 
     }
-
+    if(containerDna.isexistName(dnaName))
+    {
+        temp<<containerDna.findInNameMap(dnaName)->getCountName();
+        containerDna.findInNameMap(dnaName)->setCountName();
+        dnaName = dnaName+"_"+temp.str();
+    }
     Dna* newdna = new Dna(dnaName,"new",myfile);
     containerDna.addDna(newdna);
     print(writer,containerDna);
@@ -47,22 +53,22 @@ void Load::print(const Iwriter& writer, dataDNA& containerDna)const
 {
 
     std::stringstream temp;
-    temp<<containerDna.getMap()[Dna::getId()]->getId();
+    temp<<containerDna.findInIdMap(Dna::getId())->getId();
     std::string strId =temp.str();
 
 
 
-    if(containerDna.getMap()[Dna::getId()]->getDna().length()<40)
+    if(containerDna.findInIdMap(Dna::getId())->getDna().length()<40)
     {
-        writer.write("[" + strId + "]" + containerDna.getMap()[Dna::getId()]->getName() + ":" +
-                             containerDna.getMap()[Dna::getId()]->getDna().getAsChar());
+        writer.write("[" + strId + "]" + containerDna.findInIdMap(Dna::getId())->getName() + ":" +
+                             containerDna.findInIdMap(Dna::getId())->getDna().getAsChar());
     }
     else
     {
-        std::string lastDnasequence= (containerDna.getMap()[Dna::getId()]->getDna()).getAsChar()+containerDna.getMap()[Dna::getId()]->getDna().length()-3;
-        std::string firstDnasequence(containerDna.getMap()[Dna::getId()]->getDna().getAsChar());
+        std::string lastDnasequence= (containerDna.findInIdMap(Dna::getId())->getDna()).getAsChar()+containerDna.findInIdMap(Dna::getId())->getDna().length()-3;
+        std::string firstDnasequence(containerDna.findInIdMap(Dna::getId())->getDna().getAsChar());
         firstDnasequence = firstDnasequence.substr(0,32);
-        writer.write("[" + strId + "]" + containerDna.getMap()[Dna::getId()]->getName() + ":" + firstDnasequence+"..."+lastDnasequence);
+        writer.write("[" + strId + "]" + containerDna.findInIdMap(Dna::getId())->getName() + ":" + firstDnasequence+"..."+lastDnasequence);
 
     }
 
