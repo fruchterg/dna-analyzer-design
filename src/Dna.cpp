f50ae42bfa449b9ea8f8dcca9018eaa7b122fdf6
gilifruchter <z618149941@gmail.com>
2020-07-08 13:55:16 -0700

    add functions and member count to Dna class

--- src/Dna.cpp
+++ src/Dna.cpp
@@ -6,9 +6,13 @@
 
 #include <utility>
 size_t Dna::m_id = 0;
-Dna::Dna( const std::string&  name, const std::string&  status, const Dnasequence& dnasequence):m_Dna(dnasequence),m_name(name),m_status(status)
+Dna::Dna(const std::string&  name, const std::string&  status, const Dnasequence& dnasequence):m_Dna(new Dnasequence(dnasequence)),m_name(name),m_status(status),m_countName(1)
 {
     ++m_id;
 }
 
+size_t Dna::setCountName()
+{
+    return m_countName++;
+}
 
