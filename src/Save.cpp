28c2314495bb24f3f0c89a67436b9aa6d24a4611
gilifruchter <z618149941@gmail.com>
2020-07-08 13:58:56 -0700

    check validation

--- src/Save.cpp
+++ src/Save.cpp
@@ -9,10 +9,13 @@
 Save::Save(const Paramcommand& obj)
 {
 
+        if(!isValid(obj))
+            throw std::invalid_argument("command not found");
+
 }
-bool Save::isValid(const Paramcommand& obj)
+bool Save::isValid(const Paramcommand& param)
 {
-    return true;
+    return ((2==param.getParam().size()||(param.getParam().size()==3&&(param.getParam()[2][0]=='@')))&&param.getParam()[1][0]=='#');
 }
 void Save::run(const Iwriter& writer, dataDNA& containerDna,const Paramcommand& param)
 {
@@ -23,17 +26,23 @@ void Save::run(const Iwriter& writer, dataDNA& containerDna,const Paramcommand& 
     size_t idDna;
     temp>> idDna;
 
+    if(!containerDna.isexistId(idDna))
+    {
+        std::cout<<"id of DNA not found";
+        return;
+    }
     if(param.getParam().size()<3)
     {
-        fileDnaName =containerDna.find(idDna)->getName();
+
+        fileDnaName =containerDna.findInIdMap(idDna)->getName();
 
     }
     else
     {
-        fileDnaName = param.getParam()[2];
+        fileDnaName = param.getParam()[2].substr(1);;
     }
 
     rawDnaWriter myfile(fileDnaName);
-    myfile.write(containerDna.find(idDna)->getDna().getAsChar());
+    myfile.write(containerDna.findInIdMap(idDna)->getDna().getAsChar());
 
 }
