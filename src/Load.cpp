a399f04372ace70a33610815f75992d58b94c063
gilifruchter <z618149941@gmail.com>
2020-07-11 11:40:45 -0700

    Handling in case of same name and check validation

--- src/Load.cpp
+++ src/Load.cpp
@@ -6,9 +6,10 @@
 #include <sstream>
 #include "Load.h"
 #include "rawDnaReader.h"
-
+#include "Auxiliaryfunctions.h"
 bool Load::isValid(const Paramcommand& param)
 {
+    std::cout<<"dh";
     return ((2==param.getParam().size()||(param.getParam().size()==3&&(param.getParam()[2][0]=='@'))));
 }
 
@@ -24,23 +25,39 @@ void Load::run(const Iwriter& writer, dataDNA& containerDna,const Paramcommand& 
 {
     rawDnaReader myfile(param.getParam()[1]);
     myfile.read();
+    std::string filename=param.getParam()[1];
+
     std::string dnaName;
-    std::stringstream temp;
 
     if(param.getParam().size()<3)
     {
+
         dnaName = param.getParam()[1];
+       if(containerDna.isexistName(dnaName))
+       {
+           dnaName=param.getParam()[1]+"_"+castToString(containerDna.findInNameMap(dnaName)->getCountName());
+
+       }
+        while(containerDna.isexistName(dnaName))
+        {
+
+            containerDna.findInNameMap(dnaName)->setCountName();
+            dnaName=param.getParam()[1]+"_"+castToString(containerDna.findInNameMap(dnaName)->getCountName());
+
+        }
+
     }
     else
     {
-        dnaName = param.getParam()[2].substr(1);;
+        dnaName = param.getParam()[2].substr(1);
 
     }
+
     if(containerDna.isexistName(dnaName))
     {
-        temp<<containerDna.findInNameMap(dnaName)->getCountName();
-        containerDna.findInNameMap(dnaName)->setCountName();
-        dnaName = dnaName+"_"+temp.str();
+        writer.write("This name already Exists");
+        return;
+
     }
     Dna* newdna = new Dna(dnaName,"new",myfile);
     containerDna.addDna(newdna);
@@ -52,16 +69,14 @@ void Load::run(const Iwriter& writer, dataDNA& containerDna,const Paramcommand& 
 void Load::print(const Iwriter& writer, dataDNA& containerDna)const
 {
 
-    std::stringstream temp;
-    temp<<containerDna.findInIdMap(Dna::getId())->getId();
-    std::string strId =temp.str();
+    std::string strId =castToString(containerDna.findInIdMap(Dna::getId())->getId());
 
 
 
     if(containerDna.findInIdMap(Dna::getId())->getDna().length()<40)
     {
         writer.write("[" + strId + "]" + containerDna.findInIdMap(Dna::getId())->getName() + ":" +
-                             containerDna.findInIdMap(Dna::getId())->getDna().getAsChar());
+          containerDna.findInIdMap(Dna::getId())->getDna().getAsChar());
     }
     else
     {
